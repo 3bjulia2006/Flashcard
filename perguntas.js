d93f2290baf74708426ee5a0e04a5299ad5fa051
3bjulia2006 <168477631+3bjulia2006@users.noreply.github.com>
2024-11-27 22:20:37 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Quimica',
+    'Qual é o simbolo do Zinco?',
+    'Zn'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função de uma celula?',
+    ' realizar as atividades metabólicas do corpo, como: produção de energia, armazenamento do código genético e etc'
+)
+criaCartao(
+    'Matematica',
+    'Qual é a raiz quadrada de 255?',
+    '15.9'
+)
+
+criaCartao(
+    'Educação Fisica',
+    'Qual Esporte que é jogado com 22 pessoas em campo?',
+    'Futebol'
+)
+
+criaCartao(
+    'Fisica',
+    'Oque é o raio UV',
+    'são um tipo de radiação solar altamente energética e perigosa para o corpo humano'
+)
+
+criaCartao(
+    'Português',
+    'Qual poeta diz esse ditado "Ser feliz sem motivo é a mais autêntica forma de felicidade"',
+    'Carlos Drummond de Andrade'
+)
